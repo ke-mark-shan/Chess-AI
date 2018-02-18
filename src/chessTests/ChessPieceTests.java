dee5760402c3dd1339769b77b66c6367850ceb5a
k5shan <k5shan@edu.uwaterloo.ca>
2018-02-17 19:42:20 -0500

    Set up JUnit4 test suite

---
+++ src/chessTests/ChessPieceTests.java
@@ -0,0 +1,12 @@
+package chessTests;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
+
+@RunWith(Suite.class)
+@SuiteClasses({ BishopGetMoves.class, KingGetMoves.class, KnightGetMoves.class, PawnGetMoves.class, QueenGetMoves.class,
+		RookGetMoves.class })
+public class ChessPieceTests {
+
+}
